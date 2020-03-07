356d2d57e71dee6208002f63ad0662c9378aa780
Peter Smith <peter@smiths.im>
2020-03-07 10:36:02 +0000

    Included body-parser for json conversion

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import generateRoutes from './routes';
 import database from './database';
 
@@ -6,8 +7,9 @@ import database from './database';
 const PORT = 80;
 const HOST = '0.0.0.0';
 
-// Initialise express application
+// Initialises the express application
 const app = express();
+app.use(bodyParser.json());
 
 database.sequelize.sync().then(() => {
   // Register the routes
